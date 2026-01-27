c085459ec133088cb33d7d5d2b66e67926dbea91
Luca Sonnenfroh <l.s@sf-sec.com>
2026-01-27 21:26:52 +0100

    My changes

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Sonnenfroh IT-Security | Wir prüfen, ob Ihre IT wirklich sicher ist – bevor es jemand anderes tut.`,
+	description: `IT-Sicherheits-Checks, Risikomanaging & Reporting`,
+	url: `https://sf-sec.com`, // No trailing slash!
+	name: `SF-SEC`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
