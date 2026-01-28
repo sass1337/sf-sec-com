35787026c8e71cf362fac2ccca3dfeaf0dc5bd31
Luca Sonnenfroh <l.s@sf-sec.com>
2026-01-28 12:22:55 +0100

    My changes

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Sonnenfroh IT-Security | Wir prüfen, ob Ihre IT wirklich sicher ist – bevor es jemand anderes tut.`,
-	description: `IT-Sicherheits-Checks, Risikomanaging & Reporting`,
+	title: `SF-SEC - Sonnenfroh IT-Security`,
+	description: `Wir prüfen, ob Ihre IT wirklich sicher ist – bevor es jemand anderes tut.`,
 	url: `https://sf-sec.com`, // No trailing slash!
 	name: `SF-SEC`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
