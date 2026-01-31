34ccbc7411ac89a624d215c41d01fae9f084d75c
Luca Sonnenfroh <l.s@sf-sec.com>
2026-01-31 20:22:58 +0100

    Neue Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: 'https://sf-sec.com/',
-  sitemap: true,
+  site: 'https://sf-sec.com', // Tipp: Slash am Ende ist optional, Astro regelt das
+  // sitemap: true,  <-- Diese Zeile MUSS weg, sie verursacht den 'reduce' Fehler
   integrations: [
     sitemap(), 
     icon({
