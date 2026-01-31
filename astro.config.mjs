88a76e796ad8249219b1d6a2af134ac5d86b3cec
Luca Sonnenfroh <l.s@sf-sec.com>
2026-01-31 20:55:33 +0100

    Neue Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+// import sitemap from "@astrojs/sitemap"; 
+// sitemap() aus integrations entfernen
 
 export default defineConfig({
   site: 'https://sf-sec.com',
   integrations: [
-    sitemap(), 
     icon({
       iconDir: "src/components/icons",
     })
   ],
-  vite: {
-    build: {
-      cssMinify: 'lightningcss',
-    },
-  },
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  compressHTML: true,
 });
