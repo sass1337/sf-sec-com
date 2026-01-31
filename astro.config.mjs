f3a490381fd7ef3000e4965eecfd247e69cf8d1c
Luca Sonnenfroh <l.s@sf-sec.com>
2026-01-31 20:35:54 +0100

    Neue Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,21 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: 'https://sf-sec.com', // Tipp: Slash am Ende ist optional, Astro regelt das
-  // sitemap: true,  <-- Diese Zeile MUSS weg, sie verursacht den 'reduce' Fehler
-  integrations: [
-  sitemap({
-    filter: (page) => page !== 'https://sf-sec.com/404' // Schließt die 404 aus und initialisiert die Liste korrekt
-  }), 
-  icon({
-    iconDir: "src/components/icons",
-  })
-],
-  vite: {
-    build: {
-      cssMinify: 'lightningcss',
-    },
-  },
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  compressHTML: true,
+  site: 'https://sf-sec.com/',
+  sitemap: true,
+  integrations: [
+    sitemap(), 
+    icon({
+      iconDir: "src/components/icons",
+    })
+  ],
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+    },
+  },
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
 });
